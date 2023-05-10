5684edbe5c6a3c056cde8b78d3b14f26982f39a2
Whonki <notngu0@gmail.com>
2023-05-10 12:01:51 -0600

    BULLET HELL TITLE/Enemy.py 	BULLET HELL TITLE/New_Bullets.py 	BULLET HELL TITLE/New_main.py 	OLD BULLET HELL TITLE/

---
+++ BULLET HELL TITLE/New_Player.py
@@ -0,0 +1,8 @@
+import pygame
+class Player(pygame.sprite.Sprite):
+    def __init__(self, color, width, height):
+        super().__init__()
+        self.image = pygame.Surface([width, height])
+        self.image.fill(color)
+        self.rect = self.image.get_rect
+    def 
